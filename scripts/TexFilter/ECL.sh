e97c82f0be31cd9c1994f7decbcb943e2a8b9c04
13106 <1310654263@qq.com>
2025-11-19 21:05:05 +0800

    feat: 更新长时间预测模型的参数和训练脚本

--- scripts/TexFilter/ECL.sh
+++ scripts/TexFilter/ECL.sh
@@ -24,3 +24,72 @@ python -u run.py \
   --learning_rate 0.001 \
   --des 'Exp' \
   --itr 1
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ../dataset/electricity/ \
+  --data_path electricity.csv \
+  --model_id electricity_96_196 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --enc_in 321 \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 196 \
+  --embed_size 512 \
+  --hidden_size 512 \
+  --dropout 0 \
+  --train_epochs 20 \
+  --batch_size 4 \
+  --patience 6 \
+  --learning_rate 0.001 \
+  --des 'Exp' \
+  --itr 1
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ../dataset/electricity/ \
+  --data_path electricity.csv \
+  --model_id electricity_96_336 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --enc_in 321 \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 336 \
+  --embed_size 512 \
+  --hidden_size 512 \
+  --dropout 0 \
+  --train_epochs 20 \
+  --batch_size 4 \
+  --patience 6 \
+  --learning_rate 0.001 \
+  --des 'Exp' \
+  --itr 1
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ../dataset/electricity/ \
+  --data_path electricity.csv \
+  --model_id electricity_96_720 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --enc_in 321 \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 720 \
+  --embed_size 512 \
+  --hidden_size 512 \
+  --dropout 0 \
+  --train_epochs 20 \
+  --batch_size 4 \
+  --patience 6 \
+  --learning_rate 0.001 \
+  --des 'Exp' \
+  --itr 1
--- scripts/TexFilter/Weather.sh
+++ scripts/TexFilter/Weather.sh
@@ -24,3 +24,72 @@ python -u run.py \
   --learning_rate 0.01 \
   --des 'Exp' \
   --itr 1
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path dataset/weather/ \
+  --data_path weather.csv \
+  --model_id weather_96_192 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --enc_in 21 \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 192 \
+  --embed_size 256 \
+  --hidden_size 1024 \
+  --dropout 0 \
+  --train_epochs 20 \
+  --batch_size 64 \
+  --patience 6 \
+  --learning_rate 0.01 \
+  --des 'Exp' \
+  --use_multi_gpu
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path dataset/weather/ \
+  --data_path weather.csv \
+  --model_id weather_96_336 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --enc_in 21 \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 336 \
+  --embed_size 256 \
+  --hidden_size 1024 \
+  --dropout 0 \
+  --train_epochs 20 \
+  --batch_size 64 \
+  --patience 6 \
+  --learning_rate 0.01 \
+  --des 'Exp' \
+  --use_multi_gpu
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path dataset/weather/ \
+  --data_path weather.csv \
+  --model_id weather_96_720 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --enc_in 21 \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 720 \
+  --embed_size 256 \
+  --hidden_size 1024 \
+  --dropout 0 \
+  --train_epochs 20 \
+  --batch_size 64 \
+  --patience 6 \
+  --learning_rate 0.01 \
+  --des 'Exp' \
+  --use_multi_gpu
